9447dab043fafaa84e153208d6ec89ab2d2d7532
flozo.home <github.mail@flozo.de>
2022-07-25 17:54:54 +0200

    Add DTOs LatexCommand, LatexPackage

---
+++ de.flozo.common/src/de/flozo/dto/appearance/LatexCommand.java
@@ -0,0 +1,47 @@
+package de.flozo.dto.appearance;
+
+public class LatexCommand {
+
+    private int id;
+    private String name;
+    private LatexPackage needsPackage;
+
+    public LatexCommand(int id, String name, LatexPackage needsPackage) {
+        this.id = id;
+        this.name = name;
+        this.needsPackage = needsPackage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LatexPackage getNeedsPackage() {
+        return needsPackage;
+    }
+
+    public void setNeedsPackage(LatexPackage needsPackage) {
+        this.needsPackage = needsPackage;
+    }
+
+    @Override
+    public String toString() {
+        return "LatexCommand{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", needsPackage=" + needsPackage +
+                '}';
+    }
+}
