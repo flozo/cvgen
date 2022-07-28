3c2cd1354bf1a6eadc073547af5b94bf56c21416
flozo.home <github.mail@flozo.de>
2022-07-28 12:09:19 +0200

    Preamble: add usetikzlibrary, standaloneenv, hypersetup

--- de.flozo.latex/src/de/flozo/latex/assembly/Preamble.java
+++ de.flozo.latex/src/de/flozo/latex/assembly/Preamble.java
@@ -13,16 +13,20 @@ public class Preamble {
     public static final String KEYWORD_DOCUMENTCLASS = "documentclass";
     private final DocumentClass documentClass;
     private final Map<LatexPackage, ExpressionList> packageMap;
+    private final List<String> tikzLibraries;
+    private final List<String> hyperOptions;
 
 
-    private Preamble(DocumentClass documentClass, Map<LatexPackage, ExpressionList> packageMap) {
+    private Preamble(DocumentClass documentClass, Map<LatexPackage, ExpressionList> packageMap, List<String> tikzLibraries, List<String> hyperOptions) {
         this.documentClass = documentClass;
         this.packageMap = packageMap;
+        this.tikzLibraries = tikzLibraries;
+        this.hyperOptions = hyperOptions;
     }
 
 
-    public static Preamble create(DocumentClass documentClass, PackageList packageList) {
-        return new Preamble(documentClass, packageList.getOptionMap());
+    public static Preamble create(DocumentClass documentClass, PackageList packageList, List<String> tikzLibraries, List<String> hyperOptions) {
+        return new Preamble(documentClass, packageList.getOptionMap(), tikzLibraries, hyperOptions);
     }
 
     private String getDocumentClassCommand() {
@@ -44,10 +48,34 @@ public class Preamble {
         return codeLines;
     }
 
+    private String getUseTikzLibraryCommand() {
+        return new GenericCommand.Builder("usetikzlibrary")
+                .body(tikzLibraries)
+                .bodyDelimiter(Delimiter.COMMA)
+                .build().getInline();
+    }
+
+    private String getStandaloneEnvironmentCommand() {
+        return new GenericCommand.Builder("standaloneenv")
+                .body("tikzpicture")
+                .build().getInline();
+    }
+
+    private List<String> getHypersetupCommand() {
+        return new GenericCommand.Builder("hypersetup")
+                .body(hyperOptions)
+                .bodyDelimiter(Delimiter.COMMA)
+                .indentBody(true)
+                .build().getBlock();
+    }
+
     public List<String> getPreambleCode() {
         List<String> preambleLines = new ArrayList<>();
         preambleLines.add(getDocumentClassCommand());
         preambleLines.addAll(getUsepackageBlock());
+        preambleLines.add(getUseTikzLibraryCommand());
+        preambleLines.add(getStandaloneEnvironmentCommand());
+        preambleLines.addAll(getHypersetupCommand());
         return preambleLines;
     }
 
@@ -57,6 +85,8 @@ public class Preamble {
         return "Preamble{" +
                 "documentClass=" + documentClass +
                 ", packageMap=" + packageMap +
+                ", tikzLibraries=" + tikzLibraries +
+                ", hyperOptions=" + hyperOptions +
                 '}';
     }
 }
