4b0118c27c17363aa6ce518c8f24e04d388da1ee
flozo.home <github.mail@flozo.de>
2022-08-01 18:40:32 +0200

    DocumentElement: fix Node options

--- de.flozo.cvgen/src/de/flozo/cvgen/DocumentElement.java
+++ de.flozo.cvgen/src/de/flozo/cvgen/DocumentElement.java
@@ -1,6 +1,6 @@
 package de.flozo.cvgen;
 
-import de.flozo.common.dto.appearance.ElementStyle;
+import de.flozo.common.dto.appearance.Element;
 import de.flozo.latex.core.Delimiter;
 import de.flozo.latex.core.LengthExpression;
 import de.flozo.latex.tikz.Node;
@@ -11,55 +11,51 @@ public class DocumentElement {
 
     private final String elementName;
     private final ContentElement content;
-    private final ElementStyle style;
+    private final Element element;
 
-    public DocumentElement(String elementName, ContentElement content, ElementStyle style) {
+    public DocumentElement(String elementName, ContentElement content, Element element) {
         this.elementName = elementName;
         this.content = content;
-        this.style = style;
+        this.element = element;
+    }
+
+    private Node getNode(String elementContent) {
+        return new Node.Builder(elementContent)
+                .name(elementName)
+                .position(Point.fromNumbers(element.getPosition().getLengthX().getValue(), element.getPosition().getLengthY().getValue()))
+                .anchor(element.getAnchor())
+                .minimumWidth(LengthExpression.fromLength(element.getMinimumWidth()))
+                .minimumHeight(LengthExpression.fromLength(element.getMinimumHeight()))
+                .innerXSep(LengthExpression.fromLength(element.getSeparationSpace().getInnerXSep()))
+                .innerYSep(LengthExpression.fromLength(element.getSeparationSpace().getInnerYSep()))
+                .fontSize(element.getElementStyle().getTextStyle().getFontSize())
+                .textWidth(LengthExpression.fromLength(element.getMinimumWidth()))
+                .alignment(element.getElementStyle().getTextStyle().getAlignment())
+                .textColor(element.getElementStyle().getTextStyle().getColor())
+                .lineCap(element.getElementStyle().getLineStyle().getLineCap())
+                .lineJoin(element.getElementStyle().getLineStyle().getLineJoin())
+                .dashPatternStyle(element.getElementStyle().getLineStyle().getDashPattern())
+                .drawColor(element.getElementStyle().getLineStyle().getBaseColor())
+                .fillColor(element.getElementStyle().getAreaStyle().getColor())
+                .bodyDelimiter(Delimiter.DOUBLE_BACKSLASH)
+                .build();
     }
 
     public String getElementFieldInline() {
-        return new Node.Builder(content.inline())
-                .name(elementName)
-                .position(Point.fromNumbers(style.getPosition().getLengthX().getValue(), style.getPosition().getLengthY().getValue()))
-//                .anchor(style.getAnchor())
-                .minimumWidth(LengthExpression.fromLength(style.getWidth()))
-                .minimumHeight(LengthExpression.fromLength(style.getHeight()))
-                .fillColor(style.getAreaStyle().getColor())
-                .drawColor(style.getLineStyle().getBaseColor())
-                .textColor(style.getTextStyle().getColor())
-                .textWidth(LengthExpression.fromLength(style.getWidth()))
-//                .alignment(style.getTextStyle().)
-                .fontSize(style.getTextStyle().getFontSize())
-                .bodyDelimiter(Delimiter.DOUBLE_BACKSLASH)
-                .build().getInline();
+        return getNode(content.inline()).getInline();
     }
 
     public String getElementFieldMultiline() {
-        return new Node.Builder(content.multiline())
-                .name(elementName)
-                .position(Point.fromNumbers(style.getPosition().getLengthX().getValue(), style.getPosition().getLengthY().getValue()))
-//                .anchor(style.getAnchor())
-                .minimumWidth(LengthExpression.fromLength(style.getWidth()))
-                .minimumHeight(LengthExpression.fromLength(style.getHeight()))
-                .fillColor(style.getAreaStyle().getColor())
-                .drawColor(style.getLineStyle().getBaseColor())
-                .textColor(style.getTextStyle().getColor())
-                .textWidth(LengthExpression.fromLength(style.getWidth()))
-//                .alignment(style.getTextStyle().)
-                .fontSize(style.getTextStyle().getFontSize())
-                .bodyDelimiter(Delimiter.DOUBLE_BACKSLASH)
-                .build().getInline();
+        return getNode(content.multiline()).getInline();
     }
 
 
     @Override
     public String toString() {
-        return "Element{" +
+        return "DocumentElement{" +
                 "elementName='" + elementName + '\'' +
                 ", content=" + content +
-                ", style=" + style +
+                ", element=" + element +
                 '}';
     }
 }
