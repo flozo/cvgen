c6e17343bd8b8fafca0eb4c7edb53212b210c823
flozo.home <github.mail@flozo.de>
2022-08-02 19:57:28 +0200

    Copy LatexCompilationException from LetterGen

---
+++ de.flozo.latex/src/de/flozo/latex/LatexCompilationException.java
@@ -0,0 +1,9 @@
+package de.flozo.latex;
+
+public class LatexCompilationException extends Exception {
+
+    public LatexCompilationException(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
